a2028d23cd805677d773fa18525a7fe255942776
bcvance <ben@vancenet.net>
2022-11-14 14:35:51 -0600

    migrating to postgres again

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,7 +94,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mecommerce',
         'USER': 'bcvance',
-        'PASSWORD': 'Awpar5holein!',
         'HOST': 'mecommerce-identifier.cejp3h4y4mii.us-east-2.rds.amazonaws.com',
         'PORT': '5432'
     }
@@ -197,8 +196,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-AWS_ACCESS_KEY_ID = 'AKIAZ5WJWB66FT5NDCFA'
-AWS_SECRET_ACCESS_KEY = '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'
+
 AWS_STORAGE_BUCKET_NAME = 'mecommerce-bucket'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
