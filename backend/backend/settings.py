9a2aef23fc12922b5f785a04eed0b23cdd8d22fd
bcvance <ben@vancenet.net>
2022-11-15 18:21:24 -0600

    rerun runbuild

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -202,7 +202,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'mecommerce2-bucket'
-# AWS_S3_REGION_NAME = 'us-east-2'
 # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_QUERYSTRING_AUTH = False
 # AWS_S3_SIGNATURE_VERSION = 's3v4'
