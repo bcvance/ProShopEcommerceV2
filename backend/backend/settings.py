24299ce803b28693dd6cd8f692b91465b57b4ecb
bcvance <ben@vancenet.net>
2023-11-12 14:12:48 -0600

    update allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_qs027r8*n174_=1z)^)ma!ugsoknn12@jxnd%i5x$7cgd&c!(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://mecommerce2-f662679488b5.herokuapp.com']
 
 
 # Application definition
